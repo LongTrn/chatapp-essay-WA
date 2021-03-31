c08805d075d5a4bdeb143801861a3c5a6ff4c7db
Long <longjax1401@gmail.com>
2021-03-31 22:30:37 +0700

    add socket to emit and listen msg

--- server.js
+++ server.js
@@ -1,17 +1,64 @@
 require('dotenv').config({path: './configs/.env'});
 const express = require('express')
+const path = require('path')
 const http = require('http')
 const socketio = require('socket.io')
+
+const formatMessage = require('./utils/messages')
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-app.use('/public', express.static('public'));
+const botName = 'ChatCord Bot'
+
+// app.use('/public', express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when client connects
 io.on('connection', (socket) => {
-    console.log('New WS connnection...')
+
+    socket.on('joinRoom', ({username, room}) => {
+        // Current User
+        const user = userJoin(socket.id, username, room);
+
+        // Join room
+        socket.join(user.room)
+
+        // Welcome the current user
+        socket.emit('message', formatMessage(botName, `Welcome ${user.username} to ChatCord!`))
+
+        // Broadcast when user connects
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
+
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+    })
+
+    // Listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        const user = getCurrentUser(socket.id)
+        io.to(user.room).emit('message', formatMessage(user.username, msg))
+    })
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id)
+
+        if (user) {
+            // Broadcast when user leaves
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+            
+            // Update room users when the user leaves
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            });
+        }
+    })
 })
 
 app.get('/', (req, res) => {
